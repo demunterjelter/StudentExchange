ed2c167fb3160a3a540c555b6069715e9775a010
jelter <demunterjelter@hotmail.com>
2019-10-22 11:52:36 +0200

    login works

--- js/auth.js
+++ js/auth.js
@@ -10,11 +10,35 @@ signupForm.addEventListener('submit', (e) => {
 
     //sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
-    })
+    });
+});
 
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log('user is uitgelogged');
+    });
+});
 
-})
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        //close the login modal and reset
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
